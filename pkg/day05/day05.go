c04da5aae27b609126e9f52a3c82fe7d57ad8a09
jarodlam <jarod.lam@anz.com>
2023-12-07 15:14:30 +1100

    Day 5 part 2

--- pkg/day05/day05.go
+++ pkg/day05/day05.go
@@ -1,7 +1,6 @@
 package day05
 
 import (
-	"fmt"
 	"math"
 	"sort"
 
@@ -36,61 +35,82 @@ func (m mapping) forwardMapNoCheck(value int) int {
 	return m.dest + (value - m.source)
 }
 
-func (m mapping) forwardMapInterval(i interval) []interval {
+func (m mapping) forwardMapInterval(i interval) (interval, interval, bool) {
 	mFrom := m.source
 	mTo := m.source + m.length - 1
-	if i.from < mFrom && mFrom <= i.to && i.to <= mTo {
+	if i.to < mFrom || mTo < i.from {
+		// No overlap
+		return interval{}, interval{}, false
+	} else if i.from < mFrom && mFrom <= i.to && i.to <= mTo {
 		// Right side overlap
-		return []interval{
-			{i.from, mFrom - 1},
-			{m.forwardMapNoCheck(mFrom), m.forwardMapNoCheck(i.to)},
-		}
+		// fmt.Printf("        Right side overlap with %v\n", m)
+		return interval{m.forwardMapNoCheck(mFrom), m.forwardMapNoCheck(i.to)}, interval{mFrom, i.to}, true
 	} else if mFrom <= i.from && i.from <= mTo && mTo < i.to {
 		// Left side overlap
-		return []interval{
-			{m.forwardMapNoCheck(i.from), m.forwardMapNoCheck(mTo)},
-			{mTo + 1, i.to},
-		}
-	} else if mFrom < i.from && i.to < mTo {
+		// fmt.Printf("        Left side overlap with %v\n", m)
+		return interval{m.forwardMapNoCheck(i.from), m.forwardMapNoCheck(mTo)}, interval{i.from, mTo}, true
+	} else if i.from < mFrom && mTo < i.to {
 		// Middle overlap
-		return []interval{
-			{mFrom, i.from - 1},
-			{m.forwardMapNoCheck(i.from), m.forwardMapNoCheck(i.to)},
-			{i.to + 1, mTo},
-		}
+		// fmt.Printf("        Middle overlap with %v\n", m)
+		return interval{m.forwardMapNoCheck(mFrom), m.forwardMapNoCheck(mTo)}, interval{i.from, i.to}, true
 	} else {
 		// Full overlap
-		return []interval{
-			{m.forwardMapNoCheck(i.from), m.forwardMapNoCheck(i.to)},
-		}
+		// fmt.Printf("        Full overlap with %v\n", m)
+		return interval{m.forwardMapNoCheck(i.from), m.forwardMapNoCheck(i.to)}, interval{i.from, i.to}, true
 	}
 }
 
-func mergeIntervals(ints []interval) []interval {
+func mergeIntervals(intvls []interval) []interval {
+	if len(intvls) == 0 {
+		return intvls
+	}
+
 	// Sort by from value
 	sort.Slice(
-		ints,
-		func(i, j int) bool { return ints[i].from < ints[j].from },
+		intvls,
+		func(i, j int) bool { return intvls[i].from < intvls[j].from },
 	)
 
 	// Merge
 	out := make([]interval, 0)
-	prevInt := ints[0]
-	for i := 1; i < len(ints); i++ {
+	prevInt := intvls[0]
+	for i := 1; i < len(intvls); i++ {
 		// If overlaps with prevInt, merge with prevInt
-		if ints[i].from <= prevInt.to {
-			if prevInt.to < ints[i].to {
-				prevInt.to = ints[i].to
+		if intvls[i].from <= prevInt.to {
+			if prevInt.to < intvls[i].to {
+				prevInt.to = intvls[i].to
 			}
 			continue
 		}
 
 		// No overlap, append prevInt and start working on a new int
 		out = append(out, prevInt)
-		prevInt = ints[i]
+		prevInt = intvls[i]
 	}
 	out = append(out, prevInt)
 
+	return out
+}
+
+func intervalDiff(a []interval, b interval) []interval {
+	out := make([]interval, 0)
+	for _, i := range a {
+		if i.to < b.from || b.to < i.from {
+			// No overlap, entire interval survives
+			out = append(out, i)
+		} else if i.from < b.from && b.from <= i.to && i.to <= b.to {
+			// Right side overlap
+			out = append(out, interval{i.from, b.from - 1})
+		} else if b.from <= i.from && i.from <= b.to && b.to < i.to {
+			// Left side overlap
+			out = append(out, interval{b.to + 1, i.to})
+		} else if i.from < b.from && b.to < i.to {
+			// Middle overlap
+			out = append(out, interval{i.from, b.from - 1}, interval{b.to + 1, i.to})
+		} else {
+			// Full overlap, entire interval subtracted
+		}
+	}
 	return out
 }
 
@@ -111,19 +131,38 @@ func locateSeed(mappingSets []mappingSet, seed int) int {
 }
 
 func locateMinFromSeedRange(mappingSets []mappingSet, seed interval) int {
-	fmt.Printf("Processing seed interval %v\n", seed)
+	// fmt.Printf("Processing seed interval %v\n", seed)
 	seedIntervals := []interval{seed}
 	for _, ms := range mappingSets {
+		// fmt.Printf("  Stage %d\n", i)
 		newSeedIntervals := make([]interval, 0)
-		// Check all available mappings
-		for _, m := range ms {
-			for _, intvl := range seedIntervals {
-				newSeedIntervals = append(newSeedIntervals, m.forwardMapInterval(intvl)...)
+
+		// Individually map each interval for this stage
+		for _, intvl := range seedIntervals {
+			// fmt.Printf("    Processing interval %v\n", intvl)
+			newIntvl := make([]interval, 0)
+			unmapped := []interval{intvl}
+			// Map as much as we can
+			for _, m := range ms {
+				newValue, mappedPortion, ok := m.forwardMapInterval(intvl)
+				if ok {
+					newIntvl = append(newIntvl, newValue)
+					unmapped = intervalDiff(unmapped, mappedPortion)
+				}
 			}
-			fmt.Printf("  Mapped to %v\n", newSeedIntervals)
-			seedIntervals = mergeIntervals(newSeedIntervals)
-			fmt.Printf("    Merged to %v\n", seedIntervals)
+
+			// Merge and infill the rest
+			// fmt.Printf("      Mapped to %v\n", newIntvl)
+			newIntvl = mergeIntervals(newIntvl)
+			// fmt.Printf("      Merged to %v\n", newIntvl)
+			// newIntvl = infillIntervals(newIntvl, intvl)
+			newIntvl = append(newIntvl, unmapped...)
+			// fmt.Printf("      Infilled to %v\n", newIntvl)
+			newSeedIntervals = append(newSeedIntervals, newIntvl...)
 		}
+
+		// Merge all individually mapped intervals
+		seedIntervals = mergeIntervals(newSeedIntervals)
 	}
 
 	// Find min value
@@ -180,6 +219,11 @@ func (Solution) Part1(input string) any {
 	return minLocation
 }
 
+/*
+Part2
+Stepping through example solution
+Seeds: 79
+*/
 func (Solution) Part2(input string) any {
 	// Parse
 	maps := make([]mappingSet, 7)
@@ -191,7 +235,7 @@ func (Solution) Part2(input string) any {
 		if firstLine {
 			seedNumbers := utils.FindAllInts(line)
 			for i := 0; i < len(seedNumbers); i += 2 {
-				seeds = append(seeds, interval{seedNumbers[i], seedNumbers[i] + seedNumbers[i+1]})
+				seeds = append(seeds, interval{seedNumbers[i], seedNumbers[i] + seedNumbers[i+1] - 1})
 			}
 			firstLine = false
 			return
@@ -214,8 +258,6 @@ func (Solution) Part2(input string) any {
 		// Append
 		maps[i] = append(maps[i], mapping{numbers[0], numbers[1], numbers[2]})
 	})
-
-	fmt.Println(seeds)
 
 	// Locations
 	minLocation := math.MaxInt
